e25897dbe67fb30b124ae35c0abd728654a1fa65
Simone Westphal <simone.westphal@me.com>
2015-04-29 11:33:30 +0200

    template divided in partials, added css

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
 
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
 
-  root 'application#hello'
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
